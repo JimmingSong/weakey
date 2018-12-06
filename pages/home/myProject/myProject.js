304f9d24c2210cb7bf46b92076a03d09b78c2635
wb_songjimin <wb_songjimin@zhongan.com>
2018-12-06 18:11:45 +0800

    Initial Commit

---
+++ pages/home/myProject/myProject.js
@@ -0,0 +1,19 @@
+Component({
+  data:{
+    projectList:[{
+      txt:'项目1'
+    },
+      {
+        txt: '项目2'
+      },
+      {
+        txt: '项目3'
+      },
+      {
+        txt: '项目4'
+      }
+    ]
+  },
+  properties: { /* ... */ },
+  methods: { /* ... */ }
+})
---
+++ pages/home/teamProject/teamProject.js
@@ -0,0 +1,16 @@
+//logs.js
+const util = require('../../../utils/util.js')
+
+Component({
+  data:{
+    teamProjectList:[
+      { txt: '团队项目1' },
+      { txt: '团队项目2' },
+      { txt: '团队项目3' },
+      { txt: '团队项目4' },
+      { txt: '团队项目5' },
+      { txt: '团队项目6' },
+      { txt: '团队项目7' }
+    ]
+  }
+})
